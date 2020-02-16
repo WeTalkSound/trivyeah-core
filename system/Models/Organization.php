fc81147a91d11a44e6d6066ad917da42e697966b
gabriel <nwogugabriel@gmail.com>
2020-02-16 22:27:27 +0100

    add response helper

--- system/Models/Organization.php
+++ system/Models/Organization.php
@@ -5,13 +5,14 @@ namespace System\Models;
 use Illuminate\Http\Request;
 use System\Models\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Model;
+use System\Models\Traits\IdentifiesHostname;
 use Tenancy\Identification\Contracts\Tenant;
 use Tenancy\Identification\Concerns\AllowsTenantIdentification;
 use Tenancy\Identification\Drivers\Http\Contracts\IdentifiesByHttp;
 
 class Organization extends Model implements Tenant, IdentifiesByHttp
 {
-    use UsesUuid, AllowsTenantIdentification;
+    use UsesUuid, AllowsTenantIdentification, IdentifiesHostname;
 
     protected $fillable = [
         "name", "email"
@@ -20,19 +21,5 @@ class Organization extends Model implements Tenant, IdentifiesByHttp
     public function hostNames()
     {
         return $this->hasMany(Hostname::class);
-    }
-
-    /**
-     * Specify whether the tenant model is matching the request.
-     *
-     * @param Request $request
-     *
-     * @return Tenant
-     */
-    public function tenantIdentificationByHttp(Request $request): ?Tenant
-    {
-        return $this->whereHas("hostNames", function ($hostname) use ($request) {
-            return $hostname->where("fqdn", $request->getHttpHost());
-        })->first();
     }
 }
--- system/Models/Traits/IdentifiesHostname.php
+++ system/Models/Traits/IdentifiesHostname.php
@@ -6,7 +6,8 @@ use System\Models\Hostname;
 use Illuminate\Http\Request;
 use Tenancy\Identification\Contracts\Tenant;
 
-trait IdentifiesHostname {
+trait IdentifiesHostname
+{
     /**
      * Specify whether the tenant model is matching the request.
      *
@@ -14,12 +15,10 @@ trait IdentifiesHostname {
      *
      * @return Tenant
      */
-    public function tenantIdentificationByHttp(Request $request): ?Tenant {
-        $currentHost = $request->getHttpHost();
-        $hostname = Hostname::where('fqdn', $currentHost);
-        if ($hostname) {
-            return $hostname->organization;
-        }
-        return null;
+    public function tenantIdentificationByHttp(Request $request): ?Tenant
+    {
+        return $this->whereHas("hostNames", function ($hostname) use ($request) {
+            return $hostname->where("fqdn", $request->getHttpHost());
+        })->first();
     }
 }
